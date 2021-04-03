6d4e491cd51a49d268d329782b6f565b175bffcc
Nickolas Kenyeres <nickolas@knicklabs.com>
2021-04-03 16:56:33 -0400

    Update title on link

--- components/Card.js
+++ components/Card.js
@@ -78,7 +78,7 @@ export const Card = ({
   const fileMarkup = file
     ? (
         <div className="flex-shrink-0 ml-3">
-          <a href={file} download tabIndex="-1" title={`${linkType === 'file' ? 'Download' : 'Visit'}: ${title}`}>
+          <a href={file} download tabIndex="-1" title={`Download: ${title}`}>
             <span className="rounded-lg inline-flex p-3 bg-green-50 hover:bg-green-100 text-green-700 hover:text-green-800 ring-4 ring-white">
               <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
